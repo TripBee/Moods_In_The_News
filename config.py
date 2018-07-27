8bad287b7b9aeeb35e9e86c2841e4ed3ed552e5e
Trip Beernink <tripbeernink@Trips-MacBook.local>
2018-07-27 13:24:10 -0700

    Finish script, add config file to import API Keys, run script to recieve putput file

---
+++ config.py
@@ -0,0 +1,9 @@
+config = "title"
+
+consumer_key = "6UlSwcHdi2t7mrcFn8M1bijZz"
+
+consumer_secret = "ygb2SD5cszwwZ9Os4n7XgA9YqgM7rP4cIDXukTThEoOTNEt2wt"
+
+access_token = "760563746624249856-o1QsIf6WNyJGhi0A9lkzk9kVvVqgPda"
+
+access_token_secret = "xzEZs2GGIXLLjpmtlNOZ9fH43pHXD3fkuiReDUnFedHIj"
